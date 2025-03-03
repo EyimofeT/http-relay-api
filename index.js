a10a8906b67857416905d5b70259af1a1f4ab89d
think-dev-grow <developer@leapsail.com.ng>
2025-03-03 11:31:22 +0100

    Update Logging

--- index.js
+++ index.js
@@ -36,7 +36,11 @@ app.post('/forward', async (req, res) => {
     // Make the HTTP request using Axios
     const response = await axios(config);
 
-    // console.log(response.data)
+    console.log("Request : ")
+    console.log(req.body)
+
+    console.log("Response : ")
+    console.log(response.data)
 
     // Return the response received from the forwarded request
     res.json({
